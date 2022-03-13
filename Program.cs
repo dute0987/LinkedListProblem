de62522d02d7c136f02423084b67a71cb0e29ce3
dute0987 <dute0987@gmail.com>
2022-03-13 21:06:36 +0530

    Vishal Dute- Program.cs file Craetion of Appending Linked List

--- Program.cs
+++ Program.cs
@@ -1,14 +1,56 @@
-﻿using System;
+using LinkedList;
+using System;
+using System.Collections.Generic;
 
 namespace LinkedList
-
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int Choice, data, p;
 
-            Console.WriteLine("Welcome to Linked List Problem");
+            Console.WriteLine("Welcome to Linked List operation");
+
+            LinkList list = new LinkList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+            while (true)
+            {
+                Console.WriteLine("1.enter the element to inserted");
+                Console.WriteLine("2.Display List");
+
+                Console.WriteLine("enter your Choice=");
+                Choice = Convert.ToInt32(Console.ReadLine());
+
+                if (Choice == 3)
+                    break;
+                switch (Choice)
+                {
+                    case 1:
+                        Console.WriteLine("enter the element to inserted");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtLast(data);
+                        break;
+                    case 2:
+                        list.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the element to searched");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAtBegining(data);
+                        break;
+                    default:
+                        Console.WriteLine("wrong choice");
+                        break;
+
+                }
+                Console.WriteLine("Exit");
+            }
+
         }
     }
 }
